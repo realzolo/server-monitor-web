2fa2a6b754174afd23956b8c75536712d484b563
realzolo <zolo@onezol.com>
2022-03-08 12:30:41 +0800

    style: update the UI

--- src/components/List.js
+++ src/components/List.js
@@ -3,6 +3,7 @@ import { Table, Descriptions } from '@arco-design/web-react';
 import { IconRight, IconDown } from '@arco-design/web-react/icon';
 import { useTranslation } from 'react-i18next'
 import { bytesToSize } from "../utils/calc_bytes";
+import Loading from "./Loading";
 import {
     getProtocol,
     getUptime,
@@ -25,7 +26,7 @@ function List({ config }) {
         const websocket = new WebSocket(config.WEBSOCKET_URL);
         websocket.onmessage = function (message) {
             const json = JSON.parse(message.data);
-            setData(json);  
+            setData(json);
             setState({ loading: false, hasError: false });
         }
         websocket.onerror = function () {
@@ -42,17 +43,23 @@ function List({ config }) {
             align: "center",
             render: (col, record) => <>{getProtocol(record)}</>
         },
-        { title: t('column.name'), align: "center", dataIndex: 'name' },
+        {
+            title: t('column.name'),
+            align: "center",
+            dataIndex: 'name'
+        },
         {
             title: t('column.os'),
             align: "center",
             render: (col, record) => (
-                <img src={`https://image.onezol.com/os/${(record.platform)?.toLowerCase()}.png`} className="os_column" alt="" />
+                <img src={`https://image.onezol.com/os/${(record.platform)?.toLowerCase()}.png`} width="20px" alt="" />
             )
         },
         {
-            title: t('column.location'), align: "center", render: (col, record) => (
-                <img src={`https://image.onezol.com/flag/${record.location}.png`} className="location_column" alt="" />
+            title: t('column.location'),
+            align: "center",
+            render: (col, record) => (
+                <img src={`https://image.onezol.com/flag/${record.location}.png`} style={{ width: "24px", borderRadius: "3px" }} alt="" />
             )
         },
         {
@@ -74,19 +81,22 @@ function List({ config }) {
         {
             title: t('column.total_bytes'),
             align: "center",
-            width: "140px",
+            className: "bytes_column",
             render: (col, record) => <>{getTraffic(record)}</>
         },
         {
             title: t('column.cpu'),
+            className: "cpu_column",
             render: (col, record) => <>{getCPU(record)}</>
         },
         {
             title: t('column.memory'),
+            align: "center",
             render: (col, record) => <>{getMemory(record)}</>
         },
         {
             title: t('column.hard_disk'),
+            align: "center",
             render: (col, record) => <>{getHDD(record)}</>
         },
         {
@@ -107,46 +117,51 @@ function List({ config }) {
     return (
         <div className="list_wrapper">
             <div className="list_box">
-                <Table
-                    columns={columns}
-                    data={data || []}
-                    rowKey="host"
-                    pagination={false}
-                    noDataElement={
-                        <h4 style={{ margin: 0, color: "rgb(var(--gray-8))" }}>{state.loading ? "内容正在加载中..." : state.hasError ? "数据通道建立失败，请检查服务器是否出现异常！" : "这里没有数据，请先创建你的客户端哦。"}</h4>
-                    }
-                    expandedRowRender={(record) => (
-                        <Descriptions
-                            border
-                            data={
-                                [{
-                                    label: t('detail.cpu_count'),
-                                    value: record.cpu_count || 1
-                                }, {
-                                    label: t('detail.process_count'),
-                                    value: record.state ? record.process : "-",
-                                }, {
-                                    label: t('detail.swap_memory'),
-                                    value: (record.state ? bytesToSize(record.swap_mem_used, 1) : "-") + " / " + bytesToSize(record.swap_mem_total, 1),
-                                }, {
-                                    label: t('detail.memory'),
-                                    value: (record.state ? bytesToSize(record.mem_used, 1) : "-") + " / " + bytesToSize(record.mem_total, 1),
-                                }, {
-                                    label: t('detail.hard_disk'),
-                                    value: (record.state ? bytesToSize(record.hdd_used, 1) : "-") + " / " + bytesToSize(record.hdd_total, 1),
-                                }, {
-                                    label: t('detail.packet_loss'),
-                                    value: getPing(record)
-                                }]
-                            } />
-                    )}
-                    expandProps={{
-                        icon: ({ expanded, record }) => expanded
-                            ? <button onClick={() => removeExpandedRowKey(record.key)}><IconDown /></button>
-                            : <button onClick={() => addExpandedRowKey(record.key)}><IconRight /></button>,
-                        expandRowByClick: true
-                    }}
-                />
+                {
+                    state.loading ?
+                        (<Loading tip={t('tips.loading')} />)
+                        :
+                        (<Table
+                            columns={columns}
+                            data={data || []}
+                            rowKey="host"
+                            pagination={false}
+                            noDataElement={
+                                <h4 style={{ margin: 0, color: "rgb(var(--gray-8))" }}>{state.hasError ? t('tips.net_error') : t('tips.no_data')}</h4>
+                            }
+                            expandedRowRender={(record) => (
+                                <Descriptions
+                                    border
+                                    data={
+                                        [{
+                                            label: t('detail.cpu_count'),
+                                            value: record.cpu_count || 1
+                                        }, {
+                                            label: t('detail.process_count'),
+                                            value: record.state ? record.process : "-",
+                                        }, {
+                                            label: t('detail.swap_memory'),
+                                            value: (record.state ? bytesToSize(record.swap_mem_used, 1) : "-") + " / " + bytesToSize(record.swap_mem_total, 1),
+                                        }, {
+                                            label: t('detail.memory'),
+                                            value: (record.state ? bytesToSize(record.mem_used, 1) : "-") + " / " + bytesToSize(record.mem_total, 1),
+                                        }, {
+                                            label: t('detail.hard_disk'),
+                                            value: (record.state ? bytesToSize(record.hdd_used, 1) : "-") + " / " + bytesToSize(record.hdd_total, 1),
+                                        }, {
+                                            label: t('detail.packet_loss'),
+                                            value: getPing(record)
+                                        }]
+                                    } />
+                            )}
+                            expandProps={{
+                                icon: ({ expanded, record }) => expanded
+                                    ? <button onClick={() => removeExpandedRowKey(record.key)}><IconDown /></button>
+                                    : <button onClick={() => addExpandedRowKey(record.key)}><IconRight /></button>,
+                                expandRowByClick: true
+                            }}
+                        />)
+                }
             </div>
         </div>
     )
